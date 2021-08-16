b7df0acb7f44f9060df4c3a430f93e3a8dc05129
Genki Furukawa <genki.furukawa@gmail.com>
2021-08-16 23:36:10 +0900

    [clean] Customer.javaのprivateメソッドをMovie.javaに移動

--- src/main/java/refactoring/sample/Movie.java
+++ src/main/java/refactoring/sample/Movie.java
@@ -27,4 +27,34 @@ public class Movie {
     public String getTitle() {
         return this.title;
     }
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        switch (this.getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if (this.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
