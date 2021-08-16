3777045383a7fd4c4b8ee70205915f39fe8a20f4
Genki Furukawa <genki.furukawa@gmail.com>
2021-08-17 00:04:45 +0900

    [clean] Movie.javaのメソッドをPrice.javaに移動

--- src/main/java/refactoring/sample/price/Price.java
+++ src/main/java/refactoring/sample/price/Price.java
@@ -1,5 +1,29 @@
 package refactoring.sample.price;
 
+import refactoring.sample.Movie;
+
 public abstract class Price {
     public abstract int getPriceCode();
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        switch (this.getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
 }
