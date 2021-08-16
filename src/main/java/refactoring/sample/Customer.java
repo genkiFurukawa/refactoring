b3df0475e1d1969ede4b7f762102563c5c17f21c
Genki Furukawa <genki.furukawa@gmail.com>
2021-08-16 23:11:34 +0900

    [clean] Customer.javaのamountFor()の合計額の計算処理をメソッドに切り出す

--- src/main/java/refactoring/sample/Customer.java
+++ src/main/java/refactoring/sample/Customer.java
@@ -22,7 +22,6 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
 
         String result = "Rental Record for " + getName() + "¥n";
@@ -34,12 +33,19 @@ public class Customer {
 
             // この貸し出しに関する数値の表示
             result += "¥t" + rental.getMovie().getTitle() + "¥t" + rental.getCharge() + "¥n";
-            totalAmount += rental.getCharge();
         }
         // フッタ部分の追加
-        result += "Amount owed is " + totalAmount + "¥n";
+        result += "Amount owed is " + getTotalCharge() + "¥n";
         result += "You earned " + frequentRenterPoints + " frequnt renter points";
 
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        for (Rental rental : this.rentals) {
+            result += rental.getCharge();
+        }
         return result;
     }
 }
