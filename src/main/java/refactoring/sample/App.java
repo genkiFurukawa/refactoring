3f38288929854e3aa8de6e6efd32525fa56ce65b
Genki Furukawa <genki.furukawa@gmail.com>
2021-08-16 22:28:43 +0900

    update App.java

--- src/main/java/refactoring/sample/App.java
+++ src/main/java/refactoring/sample/App.java
@@ -2,6 +2,23 @@ package refactoring.sample;
 
 public class App {
     public static void main(String args[]){
-        System.out.println("App.main()");
+        System.out.println(">> App.main()");
+        Customer customer = new Customer("test");
+
+        Movie movie = new Movie("Movie.CHILDRENS", Movie.CHILDRENS);
+        Rental rental = new Rental(movie, 7);
+        customer.addRental(rental);
+
+        movie = new Movie("Movie.NEW_RELEASE", Movie.NEW_RELEASE);
+        rental = new Rental(movie, 1);
+        customer.addRental(rental);
+
+        movie = new Movie("Movie.REGULAR", Movie.REGULAR);
+        rental = new Rental(movie, 1);
+        customer.addRental(rental);
+
+        String result = customer.statement();
+        System.out.println(result);
+        System.out.println("<< App.main()");
     }
 }
